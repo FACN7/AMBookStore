92b3a49c0a68af3e08f7e784209f2af7350b6036
Ariel Elbahar <42386117+ArielLB@users.noreply.github.com>
2019-11-25 11:49:29 +0200

    remove console log

--- BookStore/controllers/user.js
+++ BookStore/controllers/user.js
@@ -2,7 +2,6 @@
 const User = require('../models/user');
 
 exports.signup = (req, res) => {
-    console.log('req.body', req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
