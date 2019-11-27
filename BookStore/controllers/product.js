bd7d6cdebcae0cef9a327ac1961200396c9dd284
ArielLB <arielelbachar@gmail.com>
2019-11-27 13:54:21 +0200

    added product and category delete and update and list all categories closes #21

--- BookStore/controllers/product.js
+++ BookStore/controllers/product.js
@@ -60,3 +60,59 @@ exports.create = (req, res) => {
         })
     })
 }
+
+exports.remove = (req, res) => {
+    const product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Product deleted successfully!"
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+        const { name, description, price, category, quantity, shipping } = fields;
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "all fields are required!"
+            });
+        }
+        //here instead of creating new product we are updating the one from the request
+        //we are using loadash - a tool to update all the fields in the product with single line of code
+        const product = req.product;
+        product = _.extend(product, fields);
+
+        if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image size should be 1MB or less!"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result);
+        })
+    })
+}
+
