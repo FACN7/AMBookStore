b411ffd50c04e5c9a07dbb72e8066c6fe6c3e893
ArielLB <arielelbachar@gmail.com>
2019-12-03 13:05:44 +0200

    added payment methods with paypal and visa using braintree and updated the cart to be unique Relates #34

--- bookstore-front/src/core/Checkout.js
+++ bookstore-front/src/core/Checkout.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Layout from './Layout'
-import { getProducts } from './apiCore';
-import Card from './Card';
+import { getProducts, getBraintreeClientToken, processPayment } from './apiCore';
 import { isAuthenticated } from '../auth/index';
+import DropIn from 'braintree-web-drop-in-react';
+import { emptyCart } from './cartHelper';
 
 const Checkout = ({ products }) => {
+
+    const [data, setData] = useState({
+        loading: false,
+        success: false,
+        clientToken: null,
+        error: "",
+        instance: {},
+        address: ""
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token).then(data => {
+            if (data.error) setData({ ...data, error: data.error })
+            else setData({ clientToken: data.clientToken })
+        })
+    }
+
+    useEffect(() => {
+        getToken(userId, token)
+    }, [])
+
 
     const getTotal = () => {
         return products.reduce((currentValue, nextValue) => {
@@ -15,7 +39,7 @@ const Checkout = ({ products }) => {
 
     const showCheckout = () => {
         return isAuthenticated() ? (
-            <button className='btn btn-success'>Checkout</button>
+            <div>{showDropIn()}</div>
         ) : (
                 <Link to='/signin'>
                     <button className="btn btn-primary">Sign in to checkout</button>
@@ -23,9 +47,75 @@ const Checkout = ({ products }) => {
             )
     }
 
+    const buy = () => {
+        setData({ loading: true })
+        //send data to backend
+        let nonce;
+        const getNonce = data.instance.requestPaymentMethod()
+            .then(data => {
+                nonce = data.nonce;
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getTotal(products)
+                }
+                processPayment(userId, token, paymentData)
+                    .then(response => {
+                        setData({ ...data, success: response.success });
+                        //empty the cart and create new order
+                        emptyCart(() => {
+                            setData({ loading: false })
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        setData({ loading: false })
+                    })
+            })
+            .catch(error => {
+                setData({ ...data, error: error.message });
+            })
+    }
+
+    const showDropIn = () => (
+        <div onBlur={() => setData({ ...data, error: "" })}>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <DropIn options={{
+                        authorization: data.clientToken,
+                        paypal: {
+                            flow: 'vault'
+                        }
+                    }} onInstance={instance => (data.instance = instance)} />
+                    <button onClick={buy} className="btn btn-success btn-block">Pay</button>
+                </div>
+            ) : null}
+        </div>
+    )
+
+    const showError = error => (
+        <div className="alert alert-danger"
+            style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = success => (
+        <div className="alert alert-info"
+            style={{ display: success ? '' : 'none' }}>
+            Thanks! Your payment was successful!
+        </div>
+    )
+
+    const showLoading = loading => (
+        loading && (<h2>Loading...</h2>)
+    )
+
     return (
         <div>
             <h2>Total:$ {getTotal()}</h2>
+            {showLoading(data.loading)}
+            {showSuccess(data.success)}
+            {showError(data.error)}
             {showCheckout()}
         </div>
     );
--- bookstore-front/src/core/apiCore.js
+++ bookstore-front/src/core/apiCore.js
@@ -81,3 +81,19 @@ export const getBraintreeClientToken = (userId, token) => {
     })
 };
 
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`/api/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    }).then(response => {
+        return response.json()
+    }).catch(err => {
+        console.log(err)
+    })
+};
+
