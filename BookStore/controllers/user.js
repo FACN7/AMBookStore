f52aa984f24f5678934677d35656b028bdc6dd35
Ariel Elbahar <42386117+ArielLB@users.noreply.github.com>
2019-12-04 18:11:08 +0200

    Update BookStore/controllers/user.js
    Co-Authored-By: Jamie Coe <jamiecoe66@gmail.com>

--- BookStore/controllers/user.js
+++ BookStore/controllers/user.js
@@ -37,7 +37,7 @@ exports.update = (req, res) => {
 }
 
 exports.addOrderToUserHistory = (req, res, next) => {
-    let history = [];
+    const history = [];
     req.body.order.products.forEach((item) => {
         history.push({
             _id: item._id,
